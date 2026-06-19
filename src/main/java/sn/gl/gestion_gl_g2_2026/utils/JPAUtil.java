3218a866ff2c077c2fa61453b081399c1574fb1e
Mouhammad Ndour <ndourmouhammad15@gmail.com>
2026-06-19 10:14:05 +0000

    Ajouter le CDI dans JPAUtil.java

--- src/main/java/sn/gl/gestion_gl_g2_2026/utils/JPAUtil.java
+++ src/main/java/sn/gl/gestion_gl_g2_2026/utils/JPAUtil.java
@@ -1,24 +1,36 @@
 package sn.gl.gestion_gl_g2_2026.utils;
 
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Disposes;
+import jakarta.enterprise.inject.Produces;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class JPAUtil {
 
     private static final String PERSISTENCE_UNIT_NAME = "PERSISTENCE";
-    private static EntityManagerFactory factory;
+    private EntityManagerFactory factory;
 
-    public static EntityManagerFactory getEntityManagerFactory() {
-        if (factory == null) {
-            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-        }
-        return factory;
+    @PostConstruct
+    public void init() {
+        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
     }
 
-    public static void shutdown() {
-        if (factory != null) {
-            factory.close();
-        }
+    @Produces
+    @RequestScoped
+    public EntityManager createEntityManager() {
+        return factory.createEntityManager();
+    }
+
+    public void closeEntityManager(@Disposes EntityManager em) {
+        if (em.isOpen()) em.close();
+    }
+    @PreDestroy
+    public void shutdown() {
+        if (factory != null) factory.close();
     }
 }
